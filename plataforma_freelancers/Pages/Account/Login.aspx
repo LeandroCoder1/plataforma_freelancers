b3013c717b903779dcbde54b2a2e256924b898d3
Dev-Leandro <leandro.dev21@gmail.com>
2025-09-02 20:33:18 -0300

    feat: adição inicial da estrutura da pagina de login

--- plataforma_freelancers/Pages/Account/Login.aspx
+++ plataforma_freelancers/Pages/Account/Login.aspx
@@ -1,14 +1,16 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="plataforma_freelancers.Account.WebForm1" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="YourProject.Pages.Login" %>
 
 <!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head runat="server">
-    <title></title>
+    <title>Login</title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
+            Usuário: <asp:TextBox ID="txtUser" runat="server"></asp:TextBox><br />
+            Senha: <asp:TextBox ID="txtPass" TextMode="Password" runat="server"></asp:TextBox><br />
+            <asp:Button ID="btnLogin" runat="server" Text="Entrar" OnClick="btnLogin_Click" />
         </div>
     </form>
 </body>
--- plataforma_freelancers/Pages/Account/Login.aspx.cs
+++ plataforma_freelancers/Pages/Account/Login.aspx.cs
@@ -1,16 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using plataforma_freelancers.Helpers;
 
 namespace plataforma_freelancers.Account
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class Login : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected void btnLogin_Click(object sender, EventArgs e)
         {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+
+            if (user == "admin" && pass == "123")
+            {
+                string token = JwtHelper.GenerateToken(user);
+                
+                var cookie = new HttpCookie("AuthToken", token)
+                {
+                    HttpOnly = true,
+                    Secure = false,
+                    Expires = DateTime.Now.AddMinutes(60)
+                };
+            }
+            else
+            {
+                Response.Write("<script>alert('Usuário ou senha inválidos, tente novamente')<script/>");
+            }
 
         }
     }
