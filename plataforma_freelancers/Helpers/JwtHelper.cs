983ad11eba7bdf554bccfb6c2530ad80e8ed4ecf
Dev-Leandro <leandro.dev21@gmail.com>
2025-09-02 20:00:27 -0300

    feat: adição do JWTHelper para JWT Authentication

---
+++ plataforma_freelancers/Helpers/JwtHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace plataforma_freelancers.Helpers
+{
+    public class JwtHelper
+    {
+        private static string secretKey = "a chave é secrata, xiu!!";
+
+        public static string GetSecretKey(string userId, string role, int expireMinutes = 60)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, userId),
+                    new Claim(ClaimTypes.Role, role)
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(expireMinutes),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+
+        public static ClaimsPrincipal ValidateToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var ValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            return TokenHandler.ValidateToken(token, ValidationParameters, out _);
+        }
+    }
+}
