eb824249a501b9c85d80d3b05a842f1d3c8cf1d6
Dev-Leandro <leandro.dev21@gmail.com>
2025-09-02 20:01:53 -0300

    feat: ajustes na estruturação do projeto

---
+++ plataforma_freelancers/Pages/Account/Logout.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace plataforma_freelancers.Account
+{
+    public partial class logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Cookies["jwt"] != null)
+            {
+                Response.Cookies["jwt"].Expires = DateTime.Now.AddDays(-1);
+            }
+
+            Response.Redirect("~/Pages/Account/Login.aspx");
+        }
+    }
+}
---
+++ plataforma_freelancers/Pages/Account/Register.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.Security;
+
+namespace plataforma_freelancers.Account
+{
+    public partial class AccountRegister : System.Web.UI.Page
+    {
+        protected void Register(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string password = FormsAuthentication.HashPasswordForStoringInConfigFile(txtPassword.Text, "SHA1");
+            string type = ddlUserType.SelectedValue;
+
+            using (SqlConnection conn = new SqlConnection(
+                System.Configuration.ConfigurationManager.ConnectionStrings["ConnDB"].ConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(
+                    "INSERT INTO Users (Name, Email, Password, UserType) VALUES (@Name, @Email, @Password, @UserType)", conn);
+
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password", password);
+                cmd.Parameters.AddWithValue("@Typo", type);
+
+                
+            cmd.ExecuteNonQuery();
+            }
+
+            lblMsg.Text = "Usuário registrado com sucesso!";
+        }
+    }
+}
